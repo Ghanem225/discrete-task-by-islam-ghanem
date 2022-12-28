ead4a2e8d96a88f69aa4b5ee4802f127db8b4639
Ghanem225 <121583227+Ghanem225@users.noreply.github.com>
2022-12-29 00:09:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, end, a = 0;
+            Console.WriteLine("Starting Value:-");
+            start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ending Value:-");
+            end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i < end; i++)
+            {
+                for (int j = 1; j < end; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        a++;
+                    }
+                }
+
+                if (a == 2)
+                {
+                    Console.WriteLine(i);
+                }
+                a = 0;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
